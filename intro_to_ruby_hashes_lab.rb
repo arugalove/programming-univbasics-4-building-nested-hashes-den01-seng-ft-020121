ff91abc003d02600e0ad5d4deb2920d99b447f1b
Steven Yi <steven.yi@berkeley.edu>
2021-01-25 23:16:56 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  {railroads: {}}
 end
 
 def monopoly_with_fourth_tier
@@ -25,5 +25,5 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-
+  {railroads: {pieces: 4, }}
 end
